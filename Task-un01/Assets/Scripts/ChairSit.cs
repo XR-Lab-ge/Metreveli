bd213b0460b7c7f7f3000ae16c3c7d5523b2f282
rawfilejson <mybadfohr@email.com>
2026-05-05 22:27:21 +0100

    Adding Components Task-un01

--- Task-un01/Assets/Scripts/ChairSit.cs
+++ Task-un01/Assets/Scripts/ChairSit.cs
@@ -5,42 +5,35 @@ using VRC.SDKBase;
 public class ChairSit : UdonSharpBehaviour
 {
     private VRCStation station;
-    private bool isSeated = false;
+
+    public bool isSeated = false;
 
     void Start()
     {
         station = GetComponent<VRCStation>();
     }
 
-    // კლიკი - ჯდება ან დგება
-    public override void Interact()
+    public void SitDown()
     {
         VRCPlayerApi player = Networking.LocalPlayer;
         if (player == null) return;
+        if (!isSeated) station.UseStation(player);
+    }
 
-        if (!isSeated)
-        {
-            station.UseStation(player);
-        }
-        else
-        {
-            station.ExitStation(player);
-        }
+    public void StandUp()
+    {
+        VRCPlayerApi player = Networking.LocalPlayer;
+        if (player == null) return;
+        if (isSeated) station.ExitStation(player);
     }
 
     public override void OnStationEntered(VRCPlayerApi player)
     {
-        if (player.isLocal)
-        {
-            isSeated = true;
-        }
+        if (player.isLocal) isSeated = true;
     }
 
     public override void OnStationExited(VRCPlayerApi player)
     {
-        if (player.isLocal)
-        {
-            isSeated = false;
-        }
+        if (player.isLocal) isSeated = false;
     }
 }
---
+++ Task-un01/Assets/Scripts/SitButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class SitButtonScript : UdonSharpBehaviour
+{
+    public ChairSit chairSit;
+
+    public override void Interact()
+    {
+        VRCPlayerApi player = Networking.LocalPlayer;
+        if (player == null) return;
+
+        if (!chairSit.isSeated)
+        {
+            chairSit.SitDown();
+        }
+        else
+        {
+            chairSit.StandUp();
+        }
+    }
+}
