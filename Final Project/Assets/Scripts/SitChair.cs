1a40946db6ceccc206472932b9d68d6e6dee3843
rawfilejson <mybadfohr@email.com>
2026-06-22 14:11:37 +0400

    Final Project

---
+++ Final Project/Assets/Scripts/SitChair.cs
@@ -0,0 +1,14 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class SitChair : UdonSharpBehaviour
+{
+    public VRCStation station;
+
+    public override void Interact()
+    {
+        station.UseStation(Networking.LocalPlayer);
+    }
+}
